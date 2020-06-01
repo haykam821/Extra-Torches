dffc6baed74a2210df4b3220642511849b30258e
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-06-01 01:01:48 -0400

    Add stone rods

--- src/main/java/io/github/haykam821/extratorches/Main.java
+++ src/main/java/io/github/haykam821/extratorches/Main.java
@@ -10,8 +10,12 @@ import net.minecraft.util.registry.Registry;
 public class Main implements ModInitializer {
 	public static final String MOD_ID = "extratorches";
 
+	private static final Identifier STONE_ROD_ID = new Identifier(MOD_ID, "stone_rod");
+	public static final Item STONE_ROD = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+
 	@Override
 	public void onInitialize() {
+		Registry.register(Registry.ITEM, STONE_ROD_ID, STONE_ROD);
 		TorchTypes.initialize();
 	}
 }
